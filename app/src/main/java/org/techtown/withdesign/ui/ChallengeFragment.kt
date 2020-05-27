88c794759adcfd82566d122c12a2c739394386cf
leejeongmin <danmin20@khu.ac.kr>
2020-05-27 11:27:49 +0900

    challenge fragment setting

--- app/src/main/java/org/techtown/withdesign/ui/ChallengeFragment.kt
+++ app/src/main/java/org/techtown/withdesign/ui/ChallengeFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.fragment_challenge.*
 import org.techtown.withdesign.R
+import org.techtown.withdesign.ui.challenge.BasicChallengeFragment
+import org.techtown.withdesign.ui.challenge.SuperChallengeFragment
 
-/**
- * A simple [Fragment] subclass.
- */
 class ChallengeFragment : Fragment() {
 
     override fun onCreateView(
@@ -20,4 +23,46 @@ class ChallengeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_challenge, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(challenge_viewPager) {
+            adapter = ChallengePageAdapter(childFragmentManager)
+            offscreenPageLimit = 2
+            addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+                override fun onPageScrollStateChanged(state: Int) {
+                }
+
+                override fun onPageScrolled(
+                    position: Int,
+                    positionOffset: Float,
+                    positionOffsetPixels: Int
+                ) {
+                }
+
+                override fun onPageSelected(position: Int) {
+                    challenge_nav.menu.getItem(position).isChecked = true
+                }
+
+            })
+            challenge_nav.setOnNavigationItemReselectedListener {
+                when (it.itemId) {
+                    R.id.super_challenge -> challenge_viewPager.currentItem = 0
+                    R.id.basic_challenge -> challenge_viewPager.currentItem = 1
+                }
+            }
+        }
+    }
+}
+
+class ChallengePageAdapter(fm: FragmentManager) :
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> SuperChallengeFragment()
+            else -> BasicChallengeFragment()
+        }
+    }
+
+    override fun getCount() = 2
 }
